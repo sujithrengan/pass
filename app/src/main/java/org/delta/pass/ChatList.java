12c001c90881dfd4658a55206876901faff028e1
sujithrengan <sujithrengan@gmail.com>
2016-02-19 01:10:23 +0530

    Refresher

--- app/src/main/java/org/delta/pass/ChatList.java
+++ app/src/main/java/org/delta/pass/ChatList.java
@@ -2,28 +2,62 @@ package org.delta.pass;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
 
 public class ChatList extends ActionBarActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_list);
-
+/*
         if (savedInstanceState == null) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             SwipeRefreshChatList fragment = new SwipeRefreshChatList();
             transaction.replace(R.id.sample_content_fragment, fragment);
             transaction.commit();
         }
+*/
 
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycle_view);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.activity_main_swipe_refresh_layout);
+        mSwipeRefreshLayout.setColorScheme(R.color.color_scheme_1_1, R.color.color_scheme_1_2,
+                R.color.color_scheme_1_3, R.color.color_scheme_1_4);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        mAdapter = new ChatListAdapter(new String[]{"1","2","3"});
+        mRecyclerView.setAdapter(mAdapter);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                refreshContent();
+            }
+        });
 
         final Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
         intent.setPackage("com.google.android.googlequicksearchbox");
@@ -31,7 +65,12 @@ public class ChatList extends ActionBarActivity {
         //startActivity(intent);
     }
 
+    private void refreshContent(){
+        mAdapter = new ChatListAdapter(Cheeses.CHEESES);
+        mRecyclerView.setAdapter(mAdapter);
+        mSwipeRefreshLayout.setRefreshing(false);
 
+        }
 
 
 
